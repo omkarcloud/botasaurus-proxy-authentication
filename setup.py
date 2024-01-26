ae45e8489325ef25b301b56c15303fedf3f694d2
noreply <noreply@users.noreply.github.com>
2024-01-26 16:57:11 +0530

    fixes

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
+import os
+import re
 from setuptools import setup
-from setuptools.command.install import install
 import subprocess
 import sys
+from importlib.metadata import distribution, PackageNotFoundError
 
 __author__ = "Chetan Jain <chetan@omkar.cloud>"
 
 
 install_requires = [
-    "javascript",
 ]
 extras_require = {}
 
@@ -20,46 +21,65 @@ def get_description():
     except:
         return None
 
+def install_npm_package(package_name):
+    """Install an npm package using a Python module, suppressing the output and handling errors."""
 
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
+    subprocess.check_call(
+            [sys.executable, "-m", "javascript", "--install", package_name]
+        )
 
-    def install_npm_package(self, package_name):
-        """Install an npm package using a Python module, suppressing the output and handling errors."""
-        try:
-            subprocess.run(
-                [sys.executable, "-m", "javascript", "--install", package_name],
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-        except Exception as e:
-            # Log the error if needed
-            pass
-            # print(f"An error occurred while installing {package_name}: {e}")
+    # This really loads it up.
+    try:
+        from javascript import require
 
-        # This really loads it up.
-        try:
-            from javascript import require
+        pkg = require(package_name)
+    except Exception as e:
+        pass
 
-            pkg = require(package_name)
-        except Exception as e:
-            pass
+def extract_number(s):
+    if isinstance(s, str):
+        # Use regular expression to find all numbers in the text
+        numbers = re.findall(r"\b\d+(?:\.\d+)?\b", s)
+        # Convert the extracted strings to floats or integers
+        ls = [float(num) if "." in num else int(num) for num in numbers]
+        
+        return ls[0] if ls else None
 
-    def run(self):
-        # Run the standard install
-        super().run()
+    if isinstance(s, int) or isinstance(s, float):
+        return s
 
-        print("Installing needed npm packages")
-        # Install each npm package
-        self.install_npm_package("proxy-chain")
+def check_node():
+   try:
+       NODE_BIN = os.environ.get('NODE_BIN') or (getattr(os.environ, "NODE_BIN") if hasattr(os.environ, "NODE_BIN") else "node")
+       node_version = subprocess.check_output([NODE_BIN, "-v"], universal_newlines=True).replace("v", "")
+       major_version = int(extract_number(node_version))
 
+       MIN_VER = 16
+       if major_version < MIN_VER:
+           print(f"Your Node.js version is {major_version}, which is less than {MIN_VER}. To use the stealth and auth proxy features of Botasaurus, you need Node.js 18, Kindly install it by visiting https://nodejs.org/")
+           sys.exit(1)
+   except Exception as e:
+       print("You do not have node installed on your system, Kindly install it by visiting https://nodejs.org/")
+       sys.exit(1)
+
+def install_javascript_package():
+    try:
+        distribution("javascript")
+    except PackageNotFoundError:
+        subprocess.check_call([sys.executable, "-m", "pip", "install", "javascript"])
+
+def pre_install():
+    check_node()
+    install_javascript_package()
+    install_npm_package("proxy-chain")
+
+pre_install()
 
 setup(
     name="botasaurus_proxy_authentication",
-    version="1.0.1",
+    version="1.0.2",
     author="Chetan Jain",
     author_email="chetan@omkar.cloud",
-    cmdclass={"install": PostInstallCommand},
     description="Proxy Server with support for SSL, proxy authentication and upstream proxy.",
     license="MIT",
     keywords=["seleniumwire proxy authentication", "proxy authentication"],
