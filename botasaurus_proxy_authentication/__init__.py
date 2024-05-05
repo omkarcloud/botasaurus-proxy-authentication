eacf4fd7afcb7d10016c47b4fbcc3c5b3619c8b5
Chetan <53407137+Chetan11-dev@users.noreply.github.com>
2024-05-05 15:09:12 +0530

    fixes

--- botasaurus_proxy_authentication/__init__.py
+++ botasaurus_proxy_authentication/__init__.py
@@ -1,12 +1,30 @@
-from javascript import require
+chain = None
 
-proxyChain = require("proxy-chain")
+def getchain():
+    global chain
+    if chain is None:
+        from javascript_fixes import require
+        chain = require("proxy-chain")
+    return chain
+
+def is_auth_proxy(proxy_url):
+    from urllib.parse import urlparse
+    parsed_proxy_url = urlparse(proxy_url)
+    # If upstream proxy requires no password, return it directly
+    if not parsed_proxy_url.username and not parsed_proxy_url.password:
+        return False
+    
+    return True
 
 def create_proxy(proxy_url):
-    return proxyChain.anonymizeProxy(proxy_url);
+    if is_auth_proxy(proxy_url):
+        return getchain().anonymizeProxy(proxy_url,  timeout=300, authInfo={})
+    else:
+        return proxy_url
 
 def close_proxy(proxy_url):
-    proxyChain.closeAnonymizedProxy(proxy_url, True)
+    if is_auth_proxy(proxy_url):
+        return getchain().closeAnonymizedProxy(proxy_url, True)
 
 def add_proxy_options(chrome_options, proxy):
     new_proxy = create_proxy(proxy)
--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-import os
-import re
 from setuptools import setup
-import subprocess
-import sys
-from setuptools.command.install import install
 
 __author__ = "Chetan Jain <chetan@omkar.cloud>"
 
 
 install_requires = [
-    "javascript",
+    "javascript_fixes",
 ]
 extras_require = {}
 
@@ -23,89 +18,9 @@ def get_description():
         return None
 
 
-def install_npm_package(package_name):
-    """Install an npm package using a Python module, suppressing the output and handling errors."""
-
-    try:
-        subprocess.run([sys.executable, "-m", "javascript", "--install", package_name], 
-                           stdout=subprocess.PIPE, stderr=subprocess.PIPE
-                           )
-
-    except Exception as e:
-        pass
-
-    # This really loads it up.
-    try:
-        from javascript import require
-
-        pkg = require(package_name)
-    except Exception as e:
-        pass
-
-
-def extract_number(s):
-    if isinstance(s, str):
-        # Use regular expression to find all numbers in the text
-        numbers = re.findall(r"\b\d+(?:\.\d+)?\b", s)
-        # Convert the extracted strings to floats or integers
-        ls = [float(num) if "." in num else int(num) for num in numbers]
-
-        return ls[0] if ls else None
-
-    if isinstance(s, int) or isinstance(s, float):
-        return s
-
-
-def check_node():
-    try:
-        NODE_BIN = os.environ.get("NODE_BIN") or (
-            getattr(os.environ, "NODE_BIN")
-            if hasattr(os.environ, "NODE_BIN")
-            else "node"
-        )
-        node_version = subprocess.check_output(
-            [NODE_BIN, "-v"], universal_newlines=True
-        ).replace("v", "")
-        major_version = int(extract_number(node_version))
-
-        MIN_VER = 16
-        if major_version < MIN_VER:
-            print(
-                f"Your Node.js version is {major_version}, which is less than {MIN_VER}. To use the stealth and auth proxy features of Botasaurus, you need Node.js 18, Kindly install it by visiting https://nodejs.org/"
-            )
-            sys.exit(1)
-    except Exception as e:
-        print(
-            "You do not have node installed on your system, Kindly install it by visiting https://nodejs.org/"
-        )
-        sys.exit(1)
-
-
-def pre_install():
-    check_node()
-
-
-def post_install():
-    install_npm_package("proxy-chain")
-
-
-class PostInstallCommand(install):
-    """Post-installation for installation mode."""
-
-
-    def run(self):
-        # Run the standard install
-        super().run()
-
-        print("Installing needed npm packages")
-        post_install()
-
-
-pre_install()
-
 setup(
     name="botasaurus_proxy_authentication",
-    version="1.0.8",
+    version='1.0.15',
     author="Chetan Jain",
     author_email="chetan@omkar.cloud",
     description="Proxy Server with support for SSL, proxy authentication and upstream proxy.",
